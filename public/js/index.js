84ec30bc781b453c400a4eb6538d60ca0f0ddd54
Ionică Bizău <bizauionica@yahoo.com>
2013-10-20 18:21:25 +0300

    Working version!

--- public/js/index.js
+++ public/js/index.js
@@ -6,13 +6,34 @@ $.getJSON("/getConfig", function (config) {
 
         for (var i = 0; i < repos.length; ++i) {
 
-            if (repos[i].fork || repos[i].name.indexOf("bind") !== -1) continue;
+            if (repos[i].fork) continue;
 
             var $clone = $(".template").clone().removeClass("template");
             $clone.find(".repo-title").text(repos[i].name);
-            $.getJSON("https://api.github.com/repos/" + config.username + "/" + repos[i].name + "/readme?access_token=c7a3f3cdaff220271767587beb487e09cf0e0df3", function (repo) {
-                $clone.find(".description").text(Base64.decode(repo.content));
-            });
+
+            (function (clone, name) {
+                $.getJSON("https://api.github.com/repos/" + config.username + "/" + name + "/readme?access_token=c7a3f3cdaff220271767587beb487e09cf0e0df3", function (repo) {
+                    var markdown = Base64.decode(repo.content);
+                    $.ajax({
+                        // type post
+                        type: "GET",
+                        // to url
+                        url: "/getHTML?markdown=" + encodeURIComponent(markdown),
+                        // set the success handler
+                        success: function (data) {
+                            clone.find(".description").html(data);
+                            $("table").addClass("table table-bordered");
+                            $("img").css("max-width", "100%");
+                        },
+                        // set the error handler
+                        error: function (data) {
+                            // get the json response
+                            data = data.responseJSON;
+                            console.error(data.error || data);
+                        }
+                    });
+                });
+            })($clone, repos[i].name);
 
             $repos.push($clone);
         }
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var http = require("http");
-
+var Url = require("url");
 var JohnnysStatic = require("johnnys-node-static");
+var qs = require("querystring");
+
 JohnnysStatic.setStaticServer({root: "./public"});
 JohnnysStatic.setRoutes({
         "/":       { "url": "/html/index.html" },
 });
+var Showdown = require('showdown');
+var converter = new Showdown.converter();
+
 
 http.createServer(function (req, res) {
      // safe serve
@@ -27,6 +32,16 @@ http.createServer(function (req, res) {
         return res.end(JSON.stringify(require("./credentials.json"), null, 4));
     }
 
+    var url = Url.parse(req.url);
+
+    if (url.pathname === "/getHTML") {
+        getFormData(req, res, function (err, formData) {
+            if (err) { return sendResponse(req, res, err, 400); }
+            var html = converter.makeHtml(formData.markdown);
+            res.end(html);
+        });
+    }
+
     // serve file
     JohnnysStatic.serveAll(req, res, function(err, result) {
         // check for error
@@ -40,3 +55,81 @@ http.createServer(function (req, res) {
 }).listen(8000);
 
 console.log("Listening on 8000");
+
+function getFormData(request, response, callback) {
+
+    if (typeof response === "function") {
+        callback = response;
+    }
+
+    // the request method must be 'POST'
+    if (request.method == 'POST') {
+
+        var body = '';
+
+        // on data
+        request.on('data', function (data) {
+            // add data to body
+            body += data;
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            if (body.length > 1e6) {
+                // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+                request.connection.destroy();
+            }
+        });
+
+        // and on end
+        request.on('end', function () {
+            // parse body
+            var POST = qs.parse(body);
+            // and callback it!
+            callback(undefined, POST);
+        });
+        return;
+    }
+
+    // if it's  a get request, maybe we have some query data
+    var queryData = Url.parse(request.url, true).query;
+
+    // if not post, callback undefined
+    callback(null, queryData);
+}
+
+/*
+ *  Send response to the client
+ * */
+function sendResponse (req, res, content, status, contentType, force) {
+
+    // if not provided, status is 200
+    status = status || 200;
+
+    // set the content type
+    contentType = contentType || "application/json";
+
+    // set headers and status code
+    res.writeHead(status, {
+        "Content-Type": contentType
+    });
+
+    // handle mongo errors
+    if (content instanceof Error) {
+        content = content.message;
+    }
+
+    // TODO Comments
+    var response = (content || {}).constructor === Object ? content : {};
+    if (typeof content === "string") {
+        response.output = content;
+        if (status !== 200) {
+            response.error = content;
+            delete response.output;
+        }
+    }
+
+    if (force) {
+        res.end(content);
+        return;
+    }
+
+    res.end(JSON.stringify(response, null, 4));
+};
